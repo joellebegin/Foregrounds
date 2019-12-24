c0a8a52e95016d97f741cce5660e70099ab70f76
joellebegin <joellebegin.6@gmail.com>
2019-12-24 15:58:50 -0500

    converting from freq to dist

--- Foregrounds.py
+++ Foregrounds.py
@@ -1,4 +1,6 @@
 import numpy as np 
+from astropy import units as u 
+from astropy.coordinates import Distance
 
 class Foregrounds:
     ''' generates 3d box with extragalactic foregrounds. Each pixel has a
@@ -23,6 +25,7 @@ class Foregrounds:
         self.f_alpha = foreground_alpha
         self.tmin = min_temp
         self.l_alpha = luminosity_alpha
+        self.NU_21CM = 1420 #MHz
 
         #foregrounds in frequency space
         self.generate_frequency_array()
@@ -62,17 +65,38 @@ class Foregrounds:
     def generate_amplitudes(self):
         '''The luminosity of each foreground, ie L in the foreground's spectrum
         T(nu) = L*nu**(-f_alpha) is drawn from a power law distribution 
-        P(x) = N*x**(l_alpha) for x > tmin, where N is a normalization factor.
+        P(x) = N*x**(-l_alpha) for x > tmin, where N is a normalization factor.
         '''
 
-        a = self.l_alpha -1 #since paretor is defined for x**(-1-a) not x**a
+        a = self.l_alpha -1 #since pareto is defined for x**(-1-a) not x**a
         m = self.tmin
         self.amps = (np.random.pareto(a, (self.n,self.n)) +1)*m 
         
+    def realspace(self):
+        '''converts the frequency space axis into real space distance'''
+        
+        self.freq_to_z() #converts nu --> z
+        self.z_to_dist() #converts z --> r
+
+    def freq_to_z(self):
+        '''for each frequency in self.nu, converts to the redshift corresponding 
+        to a 21-cm photon'''
+
+        self.z_arr = self.NU_21CM/self.nu -1
+
+    def z_to_dist(self):
+        '''for each redshift in self.z_arr, converts to the distance that that
+        corresponds to'''
+
+        self.dist_arr = Distance(unit = u.pc, z = self.z_arr)
+
 
 def main():
 
     f = Foregrounds()
+    f.freq_to_z()
+    f.z_to_dist()
+    print(f.dist_arr)
 
 if __name__=="__main__":
     main()
