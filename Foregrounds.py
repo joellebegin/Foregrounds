04b8a3ce655a664260ef56d69b98e4a21ca85a2e
joellebegin <joellebegin.6@gmail.com>
2019-12-25 14:50:00 -0500

    refactoring code a little so some of the old methods are more generally applicable

--- Foregrounds.py
+++ Foregrounds.py
@@ -1,6 +1,5 @@
 import numpy as np 
-from astropy import units as u 
-from astropy.coordinates import Distance
+from astropy.cosmology import WMAP9 as cosmo
 
 class Foregrounds:
     ''' generates 3d box with extragalactic foregrounds. Each pixel has a
@@ -38,7 +37,12 @@ class Foregrounds:
 
         return(self.frequency_space_temps)
 
-    def generate_frequency_array(self):
+    def real_space(self, central_freq = 150, L = 300):
+        
+        self.r_0 = self.z_to_dist(self.freq_to_z(central_freq))
+
+
+    def generate_frequency_array(self): #fspace
         '''generates the range of frequencies considered. Max and min frequencies
         are given by the specified bandwith, and the spacing is given by the
         size of the box, n.'''
@@ -47,7 +51,7 @@ class Foregrounds:
         self.nu_arr = np.ones((self.n, self.n, self.n))*self.nu[:,None,None]
     
 
-    def generate_foregrounds(self):
+    def generate_foregrounds(self): #fspace
         '''generates total temperature per pixel due to all foregrounds, where 
         the main axis is the frequency axis. i.e. frequency_space_temps[i] gives
         the temperatures at a frequency of self.nu[i]'''
@@ -57,19 +61,20 @@ class Foregrounds:
         
         #one loop generates temperature due to one foreground in each pixel
         for i in range(self.n_f):
-            self.add_foreground_temp()
+            self.frequency_space_temps += self.add_foreground_temp(self.nu_arr)
         
-    def add_foreground_temp(self):
+
+    def add_foreground_temp(self, freq_array): #foregrounds
         '''for each foreground, generates its spectrum T(nu) = L*nu**(-alpha)
         where L is generated in generate_amplitudes() and alpha is drawn from 
         a normal distribution.'''
 
         self.generate_amplitudes()
         alpha = np.random.normal(self.f_alpha[0],self.f_alpha[1], (self.n,self.n))
-        lnT = np.log(self.amps[None:,:]) - alpha*np.log(self.nu_arr)
-        self.frequency_space_temps += np.exp(lnT) 
+        lnT = np.log(self.amps[None:,:]) - alpha*np.log(freq_array)
+        return np.exp(lnT) 
 
-    def generate_amplitudes(self):
+    def generate_amplitudes(self): #foregrounds
         '''The luminosity of each foreground, ie L in the foreground's spectrum
         T(nu) = L*nu**(-f_alpha) is drawn from a power law distribution 
         P(x) = N*x**(-l_alpha) for x > tmin, where N is a normalization factor.
@@ -78,24 +83,14 @@ class Foregrounds:
         a = self.l_alpha -1 #since pareto is defined for x**(-1-a) not x**a
         m = self.tmin
         self.amps = (np.random.pareto(a, (self.n,self.n)) +1)*m 
-        
-    def realspace(self):
-        '''converts the frequency space axis into real space distance'''
-        
-        self.freq_to_z() #converts nu --> z
-        self.z_to_dist() #converts z --> r
 
-    def freq_to_z(self):
-        '''for each frequency in self.nu, converts to the redshift corresponding 
-        to a 21-cm photon'''
+    def freq_to_z(self, nu):
+    
+        return self.NU_21CM/nu -1
 
-        self.z_arr = self.NU_21CM/self.nu -1
-
-    def z_to_dist(self):
-        '''for each redshift in self.z_arr, converts to the distance that that
-        corresponds to'''
-
-        self.dist_arr = Distance(unit = u.pc, z = self.z_arr)
+    def z_to_dist(self, z):
+       
+        return cosmo.comoving_distance(z)
 
 
 def main():
